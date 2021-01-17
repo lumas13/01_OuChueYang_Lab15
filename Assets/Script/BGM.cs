707eab800f336bf8748c2da68f87b417e6a0c768
Eli <72849929+lumas13@users.noreply.github.com>
2021-01-17 19:17:01 +0800

    Initial commit

---
+++ Assets/Script/BGM.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    public AudioClip[] bgmClip;
+    private AudioSource audioSource;
+
+    float volume = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            int rand = Random.Range(0, bgmClip.Length);
+            audioSource.PlayOneShot(bgmClip[rand]);
+
+            if (audioSource.clip = bgmClip[0])
+            {
+                audioSource.Stop();
+                audioSource.PlayOneShot(bgmClip[rand]);
+            }
+            if (audioSource.clip = bgmClip[1])
+            {
+                audioSource.Stop();
+                audioSource.PlayOneShot(bgmClip[rand]);
+            }
+            if (audioSource.clip = bgmClip[2])
+            {
+                audioSource.Stop();
+                audioSource.PlayOneShot(bgmClip[rand]);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<AudioSource>().volume += volume * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<AudioSource>().volume -= volume * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject jumpText;
+    public AudioClip[] audioClip;
+    private AudioSource audioSource;
+
+    float jumpPower = 5f;
+    int jumpCounter = 0;
+
+    bool isOnGround = true;
+
+    Rigidbody playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
+        {
+            playerRB.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            jumpCounter++;
+            isOnGround = false;
+            jumpText.GetComponent<Text>().text = "Jumped: " + jumpCounter;
+
+            int rand = Random.Range(0, audioClip.Length);
+            audioSource.PlayOneShot(audioClip[rand]);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+}
